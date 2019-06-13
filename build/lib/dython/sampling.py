ec7e566f962e788df5dc541c829f02e0875dab39
Shaked Zychlinski <shakedzy@gmail.com>
2019-06-13 19:06:22 +0300

    docstring tab removal

--- build/lib/dython/sampling.py
+++ build/lib/dython/sampling.py
@@ -5,10 +5,16 @@ def weighted_sampling(numbers, k=1, with_replacement=False, **kwargs):
     """
     Return k numbers from a weighted-sampling over the supplied numbers
 
-    :param numbers: List or np.ndarray of numbers
-    :param k: Integer. How many numbers to sample. Choosing k=None will yield a single number
-    :param with_replacement: Boolean. Allow replacement or not
-    :return: k numbers sampled from numbers. List or np.ndarray, depending on the input. if k=None, returns a single number
+    **Returns:** List, np.ndarray or a single number (depending on the input)
+
+    Parameters
+    ----------
+    numbers : List or np.ndarray
+        numbers to sample
+    k : int, default = 1
+        How many numbers to sample. Choosing `k=None` will yield a single number
+    with_replacement : Boolean, default = False
+        Allow replacement or not
     """
     sampled = np.random.choice(numbers, size=k, replace=with_replacement)
     if (isinstance(numbers, list) or kwargs.get('to_list', False)) and k is not None:
@@ -20,10 +26,16 @@ def boltzmann_sampling(numbers, k=1, with_replacement=False):
     """
     Return k numbers from a boltzmann-sampling over the supplied numbers
 
-    :param numbers: List or np.ndarray of numbers
-    :param k: Integer. How many numbers to sample. Choosing k=None will yield a single number
-    :param with_replacement: Boolean. Allow replacement or not
-    :return: k numbers sampled from numbers. List or np.ndarray, depending on the input. if k=None, returns a single number
+    **Returns:** List, np.ndarray or a single number (depending on the input)
+
+    Parameters
+    ----------
+    numbers : List or np.ndarray
+        numbers to sample
+    k : int, default = 1
+        How many numbers to sample. Choosing `k=None` will yield a single number
+    with_replacement : Boolean, default = False
+        Allow replacement or not
     """
     exp_func = np.vectorize(lambda x: np.exp(x))
     exp_numbers = exp_func(numbers)
