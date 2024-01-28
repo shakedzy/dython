8d81e70e09009a394bbcbed791d3556f6864261e
Shaked Zychlinski <shakedzy@gmail.com>
2024-01-28 23:42:47 +0200

    Update test_split_hist.py

--- tests/test_data_utils/test_split_hist.py
+++ tests/test_data_utils/test_split_hist.py
@@ -1,5 +1,5 @@
 import pytest
-import matplotlib
+import matplotlib.pyplot as plt
 
 from dython.data_utils import split_hist
 
@@ -7,4 +7,4 @@ from dython.data_utils import split_hist
 def test_split_hist_check(iris_df):
     result = split_hist(iris_df, "sepal length (cm)", "target")
 
-    assert isinstance(result, matplotlib.axes.Axes)
+    assert isinstance(result, plt.Axes)
